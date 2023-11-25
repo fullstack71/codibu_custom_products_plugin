7d839573e0e892de6816d8bc15464df9ddc26fe5
fullstack71 <developerfullstack71@gmail.com>
2023-11-26 02:20:07 +0600

    Update load_pages.php

--- load_pages.php
+++ load_pages.php
@@ -5,4 +5,5 @@ require(spin_wheel_dir."/oopspk_spin_wheel_for_woocommerce_functions.php");
 require(spin_wheel_dir."/oopspk_spin_wheel_for_add_car_vin_number.php");
 require(spin_wheel_dir."/oopspk_spin_wheel_for_add_field_to_woocommerce_checkout.php");
 require(spin_wheel_dir."/oopspk_spin_wheel_for_select_car_bin_number.php");
+require(spin_wheel_dir."/oopspk_spin_wheel_for_order_invoice.php");
 ?>
