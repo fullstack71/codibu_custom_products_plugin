1555b2b964e67c8a5330d74fc2724a00cdc43097
saidul <developerfullstack71@gmail.com>
2024-05-29 11:08:59 -0700

    done

--- codibu_reset_cart.php
+++ codibu_reset_cart.php
@@ -1,16 +1,9 @@
 <?php
 
-function custom_enqueue_scripts() {
-    if (is_cart() || is_checkout()) {
-        wp_enqueue_script('custom-clear-cart', get_template_directory_uri() . 'assets/js/clear-cart.js', array('jquery'), null, true);
-        wp_localize_script('custom-clear-cart', 'custom_ajax_object', array('ajax_url' => admin_url('admin-ajax.php')));
+function custom_clear_cart() {
+    echo $current_url="//".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+    if(!str_contains($current_url, 'last-step')){
+        WC()->cart->empty_cart();
     }
 }
-add_action('wp_enqueue_scripts', 'custom_enqueue_scripts');
-
-function custom_clear_cart() {
-    WC()->cart->empty_cart();
-    wp_die();
-}
-add_action('wp_ajax_custom_clear_cart', 'custom_clear_cart');
-add_action('wp_ajax_nopriv_custom_clear_cart', 'custom_clear_cart');
+add_action('init', 'custom_clear_cart');
